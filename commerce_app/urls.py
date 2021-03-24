21823a32f53d2ea9395948e5e1bfeed44cdf0b87
Jennifer Cunningham <cptcunningham@gmail.com>
2021-03-23 17:57:35 -0700

    fixed login and registration url paths

--- commerce_app/urls.py
+++ commerce_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login', views.login),
+    path('logging', views.logging),
     path('register', views.register),
     path('cart', views.cart),
     path('purchase', views.purchase),
